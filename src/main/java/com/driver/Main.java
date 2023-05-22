1ed945b00754010dca3d56ab6489054d47ee505c
Your Name <you@example.com>
2023-05-22 09:06:43 +0530

    third

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,30 @@
 package com.driver;
 
 public class Main {
-  
+    public Main() {
+    }
+
+    public static void main(String[] args) {
+        B check = new B();
+        check.meth();
+        check.meth();
+    }
+
+    public static class B extends A {
+        public B() {
+        }
+
+        public String meth() {
+            return "Method is overridden in Extendend class B";
+        }
+    }
+
+    public static class A {
+        public A() {
+        }
+
+        public String meth() {
+            return "Invoking method from class A";
+        }
+    }
 }
